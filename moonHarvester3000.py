51778d15ef7d27e9442eea2dfe635794e95f57e9
jonahanderson <jonahmanderson10@gmail.com>
2024-07-19 00:34:39 -0400

    updates to prompt and fine tuned model deployment

--- moonHarvester3000.py
+++ moonHarvester3000.py
@@ -89,17 +89,17 @@ def mark_post_as_processed(submission):
 fine_tuned_model_id = os.getenv('FINE_TUNED_MODEL_ID')
 
 def generate_comments(post_title, post_text):
-    prompt = f"Generate a comment for the following Reddit post:\n\nTitle: {post_title}\n\nText: {post_text}\n\nPlease ensure the comment is in lowercase, does not contain quotes, and does not contain any emojis or special characters. 30% of the time you create a negative, nuanced comment. 10% of the time you use 1-10 words."
+    prompt = f"Generate a comment for the following Reddit post:\n\nTitle: {post_title}\n\nText: {post_text}\n\n"
     
     try:
         completion = openai.chat.completions.create(
             model=fine_tuned_model_id,
             messages=[
-                {"role": "system", "content": "You are a versatile Reddit commenter aiming to maximize karma on the cryptocurrency subreddit. Your comments should be engaging and tailored to the context—sometimes informative, sometimes humorous or sarcastic, and occasionally provocative. Always consider what type of comment will get the most upvotes in each situation. Ensure the comments are in lowercase and do not include any emojis or special characters. Do not overdo it on commas or punctuation."},
+                {"role": "system", "content": "You are a versatile Reddit commenter aiming to maximize karma on the cryptocurrency subreddit. Your comments should be engaging and tailored to the context—sometimes informative, sometimes humorous or sarcastic, and occasionally provocative. Always consider what type of comment will get the most upvotes in each situation."},
                 {"role": "user", "content": prompt}
             ],
             max_tokens=350,
-            temperature=1.0,  # Increased temperature for more variability
+            temperature=0.7,  # Increased temperature for more variability
             n=3  # Request three completions
         )
         comments = [choice.message.content.strip() for choice in completion.choices]
